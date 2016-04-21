02e3845159002d18a62afa34141e10c25a5fb6ec
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2016-04-21 12:58:27 +0200

    Imported sounds. connected sounds to monster. Imported mask. Created a prefab of man and mask

---
+++ Assets/Scripts/Howl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Howl : MonoBehaviour {
+	private int timer;
+	public AudioClip howlSound;
+
+	// Use this for initialization
+	void Start () {
+		timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer++;
+		if(timer>=100){
+			AudioSource.PlayClipAtPoint(howlSound, transform.position);
+			timer = 0;
+		}
+	}
+}
