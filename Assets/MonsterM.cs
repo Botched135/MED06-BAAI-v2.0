815290c5a1df68790f36ed6a88def8f1589b5c35
Botched135 <rasmusbons@gmail.com>
2016-04-21 15:43:37 +0200

    All monsters are made into the scenes but we are missing the animation for Marvelous monster

--- Assets/MonsterM.cs
+++ Assets/MonsterM.cs
@@ -5,11 +5,73 @@ public class MonsterM : Monster {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        anim = GetComponent<Animator>();
+        Directions = GameObject.FindGameObjectsWithTag("Front");
+        wayPointIndex = 0;
+        wayPoints = new GameObject[2];
+        nav = GetComponent<NavMeshAgent>();
+        nav.speed = 1f;
+
+
+        rotationTime = 3f;
+
+        wayPoints = GameObject.FindGameObjectsWithTag("Point1");
+        player = GameObject.FindGameObjectWithTag("Player");
+        time = 0;
+        nav.destination = wayPoints[0].transform.position;
+
+        pivotPoint = GameObject.FindGameObjectWithTag("TransformPos");
+
+        //EnemySight
+        seenPlayer = false;
+        anim.SetBool("SeenPlayer", false);
+        col = GetComponent<SphereCollider>();
+    }
+    public void OnTriggerStay(Collider other) //EnemySight - should use base keyword in children classes
+    {
+        if (other.gameObject == player)
+        {
+            direction = other.transform.position - transform.position;
+            angle = Vector3.Angle(direction, transform.forward);
+
+            if (angle < fieldOfViewAngle * 0.5f)
+            {
+                Debug.DrawRay(transform.position + direction.normalized, direction, Color.red);
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position + direction.normalized, direction, out hit, col.radius))
+                {
+                    if (hit.collider.gameObject == player)
+                    {
+
+                        seenPlayer = true;
+                        StopCoroutine(LookingSequence());
+
+                    }
+                }
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+        moveSpeed = nav.velocity.sqrMagnitude;
+        anim.SetFloat("Speed", moveSpeed * moveSpeed);
+        if (seenPlayer)
+        {
+            playerPosition = player.transform.position;
+            anim.SetBool("SeenPlayer", true);
+            nav.speed = 2;
+            NoticePlayer();
+
+
+            nav.SetDestination(playerPosition);
+            nav.Resume();
+            Attack();
+        }
+        else if (!seenPlayer)
+            Move();
+    }
+    public override void NoticePlayer()
+    {
+       //play sound
+    }
 }
