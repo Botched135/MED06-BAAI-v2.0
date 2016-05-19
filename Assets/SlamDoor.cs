35d50efd30d72f6b8eebe280417a6827dc07041e
Botched135 <rasmusbons@gmail.com>
2016-05-19 10:01:48 +0200

    IronMaiden and slam door update

--- Assets/SlamDoor.cs
+++ Assets/SlamDoor.cs
@@ -5,6 +5,7 @@ public class SlamDoor : MonoBehaviour {
 
     public GameObject Door;
     public AudioClip SlamSound;
+    public AudioClip Laugh;
     public GameAI AI;
     private GameObject Player;
     private BoxCollider Trigger;
@@ -22,10 +23,17 @@ public class SlamDoor : MonoBehaviour {
         if(other.gameObject == Player)
         {
             Door.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 0, transform.localEulerAngles.x);
-            AudioSource.PlayClipAtPoint(SlamSound, Door.transform.position, 1.0F);
+            Door.GetComponent<OpenDoorStright>().enabled = false;
+            AudioSource.PlayClipAtPoint(SlamSound, new Vector3(Door.transform.position.x+1, Door.transform.position.y+1, Door.transform.position.z),110f);
+            StartCoroutine(Laughter());
             AI.SaveToFile(AI.time, 2);
             Destroy(Trigger);
             Destroy(gameObject);
         }
+    }
+    IEnumerator Laughter()
+    {
+        yield return new WaitForSeconds(2f);
+        AudioSource.PlayClipAtPoint(Laugh, new Vector3(Door.transform.position.x + 1, Door.transform.position.y + 1, Door.transform.position.z), 110f);
     }
 }
